4538951065a595abc52e46c3f79b078c72c1c5ce
khofmans <kris@rotateit.be>
2021-02-14 10:11:35 +0100

    Test to prevent finished items from changing

--- application/app/src/test/kotlin/io/tripled/todo/UpdateInformationInTodoItemTest.kt
+++ application/app/src/test/kotlin/io/tripled/todo/UpdateInformationInTodoItemTest.kt
@@ -1,5 +1,6 @@
 package io.tripled.todo
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import io.tripled.todo.mothers.Todos
 import io.tripled.todo.testing.TodoItemTest
@@ -9,12 +10,12 @@ class UpdateInformationInTodoItemTest : TodoItemTest({ fakeTodoItems, testTodoIt
     val updateInformationInTodoItem: UpdateInformationInTodoItem = UpdateInformationInTodoItemCommand(fakeTodoItems)
 
     given("A todo item that's in progress") {
-        fakeTodoItems.assumeExisting = Todos.paintingTheRoom
         val request = UpdateInformationInTodoItem.Request(Todos.paintingTheRoom.id,
                                                           "Paint living room",
                                                           "Paint living room pink")
 
         `when`("We update the information inside the todo item") {
+            fakeTodoItems.assumeExisting = Todos.paintingTheRoom
             updateInformationInTodoItem.updateInformation(request)
 
             then("We verify that the todo item has been updated") {
@@ -22,6 +23,18 @@ class UpdateInformationInTodoItemTest : TodoItemTest({ fakeTodoItems, testTodoIt
                     title = "Paint living room",
                     description = "Paint living room pink",
                 )
+            }
+        }
+
+        `when`("We attempt to update a finished todo item") {
+            fakeTodoItems.assumeExisting = Todos.finishedPaintingTheRoom
+
+            val exception = shouldThrow<DomainException> {
+                updateInformationInTodoItem.updateInformation(request)
+            }
+
+            then("It should fail since we don't want them to change anymore") {
+                exception shouldBe DomainException("Can't change todoItem '${Todos.paintingTheRoom.id.id}' it's 'FINISHED'")
             }
         }
     }
