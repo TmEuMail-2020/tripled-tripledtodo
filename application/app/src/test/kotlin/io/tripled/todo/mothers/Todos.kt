dc769b0ce155813ab94aa33985726c4b9051df77
khofmans <kris@rotateit.be>
2021-02-14 19:54:35 +0100

    Write test for the first query in the system: GetTodoItems

--- application/app/src/test/kotlin/io/tripled/todo/mothers/Todos.kt
+++ application/app/src/test/kotlin/io/tripled/todo/mothers/Todos.kt
@@ -12,7 +12,15 @@ class Todos {
             TodoItem.Status.CREATED,
         )
 
+
         val cancelledPaintingTheRoom = paintingTheRoom.copy(status = TodoItem.Status.CANCELLED)
         val finishedPaintingTheRoom  = paintingTheRoom.copy(status = TodoItem.Status.FINISHED)
+
+        val cleaningTheGutter = TodoItem.Snapshot(
+            TodoId.existing("xyz-987"),
+            "Cleaning gutter",
+            "Gutters need to be cleaned on the front and back side of the house",
+            TodoItem.Status.CREATED,
+        )
     }
 }
---
+++ application/app/src/test/kotlin/io/tripled/todo/query/GetTodoItemsTest.kt
@@ -0,0 +1,37 @@
+package io.tripled.todo.query
+
+import io.kotest.matchers.shouldBe
+import io.tripled.todo.TodoId
+import io.tripled.todo.domain.TodoItem
+import io.tripled.todo.mothers.Todos
+import io.tripled.todo.testing.TodoItemTest
+
+
+class GetTodoItemsTest : TodoItemTest({ fakeTodoItems, testTodoItems ->
+    val getTodoItems: GetTodoItems = GetTodoItemsQuery(fakeTodoItems)
+
+    given("Some todo items") {
+        fakeTodoItems.assumeMultipleExisting = asList(Todos.paintingTheRoom, Todos.cleaningTheGutter)
+
+        `when`("Getting them") {
+            val response = getTodoItems.getAll()
+
+            then("We should have gotten them all") {
+                response.todoItems shouldBe listOf(
+                    GetTodoItems.Response.TodoItem(
+                        TodoId.existing("abc-123"),
+                        "Painting",
+                        "Paint living room white",
+                        TodoItem.Status.CREATED,
+                    ),
+                    GetTodoItems.Response.TodoItem(
+                        TodoId.existing("xyz-987"),
+                        "Cleaning gutter",
+                        "Gutters need to be cleaned on the front and back side of the house",
+                        TodoItem.Status.CREATED,
+                    )
+                )
+            }
+        }
+    }
+})
