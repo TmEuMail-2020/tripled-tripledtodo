0edec899e8bc81adfb0073c3b9cef690b3888158
khofmans <kris@rotateit.be>
2021-02-20 01:03:17 +0100

    Initial work on aggregated test coverage report

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,17 +5,73 @@ plugins {
 	val springDependencyManagementVersion = "1.0.10.RELEASE"
 
 	idea
+	jacoco
 	kotlin("jvm") version kotlinVersion apply false
 	kotlin("plugin.spring") version kotlinVersion apply false
 	id("io.spring.dependency-management") version springDependencyManagementVersion
-	id("jacoco")
 	id("io.gitlab.arturbosch.detekt").version("1.16.0-RC1")
+	id("org.kordamp.gradle.kotlin-project") version "0.43.0"
+	id("org.kordamp.gradle.coveralls") version "0.43.0"
+	id("org.kordamp.gradle.jacoco") version "0.43.0"
+
 }
+
+config {
+
+	release = false
+
+	info {
+		name = "triple-todo"
+		description = "Example application for clean architecture, ddd, cqrs, tdd, etc"
+		inceptionYear = "2020"
+		vendor = "tripled"
+		group = "io.tripled"
+
+		links {
+			website = "https://www.tripled.io"
+			scm = "https://gitlab.rotate-it.be/tripled/triple-todo"
+			issueTracker = "https://gitlab.rotate-it.be/tripled/triple-todo/-/issues"
+		}
+
+		licensing {
+			licenses {
+				license {
+					id = "Apache-2.0"
+				}
+			}
+		}
+
+		coverage {
+			jacoco {
+				enabled = true
+				aggregateReportHtmlFile = File("build/reports/index.html")
+			}
+			coveralls {
+				enabled = true
+			}
+		}
+
+		people {
+			person {
+				id = "gert"
+				name = "Gert Vilain"
+				email = "gert@tripled.io"
+				roles = listOf("developer")
+			}
+			person {
+				id = "kris"
+				name = "Kris Hofmans"
+				email = "kris@tripled.io"
+				roles = listOf("author", "developer")
+			}
+		}
+	}
+}
+
 
 allprojects {
 	group = "io.tripled"
 	version = "0.0.1-SNAPSHOT"
-
 
 	repositories {
 		mavenCentral()
