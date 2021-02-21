211f492a3082574acea31d886b2e821b351e92d0
khofmans <kris@rotateit.be>
2021-02-21 12:26:18 +0100

    A test to verify the ability to delegate

--- code/infrastructure/incoming/validation/src/test/java/io/tripled/todo/infra/validation/CreateTodoItemValidatorTest.kt
+++ code/infrastructure/incoming/validation/src/test/java/io/tripled/todo/infra/validation/CreateTodoItemValidatorTest.kt
@@ -1,5 +1,6 @@
 package io.tripled.todo.infra.validation
 
+import io.tripled.todo.TodoId
 import io.tripled.todo.command.CreateTodoItem
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.fail
@@ -28,6 +29,32 @@ class CreateTodoItemValidatorTest {
 					ValidationException.Validation("description", "should not be empty"),
 				)
 			)
+		}
+	}
+
+
+	@Test
+	fun `a valid request to create a todo gets validated`() {
+		// given: a valid request
+		val delegatee = Delegatee(CreateTodoItem.Response(TodoId("delegatee-response")))
+		val request = CreateTodoItem.Request("a title", "a description")
+
+		// when
+		val response = CreateTodoItemValidator(delegatee).create(request)
+
+		// then
+		assertThat(delegatee.calledWithRequest)
+			.isEqualTo(CreateTodoItem.Request("a title", "a description"))
+		assertThat(response)
+			.isEqualTo(CreateTodoItem.Response(TodoId("delegatee-response")))
+
+	}
+
+	class Delegatee(val response: CreateTodoItem.Response) : CreateTodoItem {
+		lateinit var calledWithRequest: CreateTodoItem.Request
+		override fun create(request: CreateTodoItem.Request): CreateTodoItem.Response {
+			calledWithRequest = request
+			return response
 		}
 	}
 
