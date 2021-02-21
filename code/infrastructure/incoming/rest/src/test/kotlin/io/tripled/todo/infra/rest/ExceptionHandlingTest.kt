a0f0ce97067cbae5151b8ff09b814a2c7f15cff4
khofmans <kris@rotateit.be>
2021-02-21 20:56:27 +0100

    Write test for domain exception handling

--- code/infrastructure/incoming/rest/src/test/kotlin/io/tripled/todo/infra/rest/ExceptionHandlingTest.kt
+++ code/infrastructure/incoming/rest/src/test/kotlin/io/tripled/todo/infra/rest/ExceptionHandlingTest.kt
@@ -1,5 +1,6 @@
 package io.tripled.todo.infra.rest
 
+import io.tripled.todo.DomainException
 import io.tripled.todo.ValidationException
 import io.tripled.todo.infra.rest.testing.RestTest
 import org.junit.jupiter.api.Test
@@ -10,6 +11,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.skyscreamer.jsonassert.JSONAssert.assertEquals
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 
 @Import(value = [ExceptionHandlingTest.ErrorsRestTestController::class])
@@ -35,7 +37,21 @@ class ExceptionHandlingTest : RestTest() {
 				  "message" : "something else wrong"
 				} 
 				]""", false)
+	}
 
+	@Test
+	fun `when receiving a domain exception we get a bad request`() {
+		val result = this.mockMvc
+			.perform(
+				get("/test/api/domainError")
+			)
+			.andDo(print())
+			.andExpect(
+				MockMvcResultMatchers.content().string("""{
+  "message" : "invariant violated"
+}"""))
+			.andExpect(status().isBadRequest)
+			.andReturn()
 	}
 
 	@RestController
@@ -46,6 +62,10 @@ class ExceptionHandlingTest : RestTest() {
 				ValidationException.Validation("testField1", "something wrong"),
 				ValidationException.Validation("testField2", "something else wrong"),
 			))
+		}
+		@GetMapping("/test/api/domainError")
+		fun triggerDomainException(){
+			throw DomainException("invariant violated")
 		}
 	}
 }
