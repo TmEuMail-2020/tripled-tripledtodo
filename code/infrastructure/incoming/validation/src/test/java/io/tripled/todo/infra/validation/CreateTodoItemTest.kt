2148b6ba5455a5878d1ebd155b1d34673f2cd6d7
khofmans <kris@rotateit.be>
2021-02-21 10:14:52 +0100

    Introduce test for initial validation case

---
+++ code/infrastructure/incoming/validation/src/test/java/io/tripled/todo/infra/validation/CreateTodoItemTest.kt
@@ -0,0 +1,34 @@
+package io.tripled.todo.infra.validation
+
+import io.tripled.todo.command.CreateTodoItem
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.fail
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+class CreateTodoItemTest {
+
+	@Test
+	fun `a request to create a todo gets validated`() {
+		// given: an invalid request
+		val request = CreateTodoItem.Request("", "")
+
+		// when
+		try {
+			CreateTodoItemValidator().create(request)
+			fail("Should not pass without validation")
+		} catch (ValidationException ve){
+			assertThat(ve).isEqualTo(
+				ValidationException(
+					ValidationException.Validation("title", "should not be empty"),
+					ValidationException.Validation("description", "should not be empty"),
+				)
+			)
+		}
+	}
+}
