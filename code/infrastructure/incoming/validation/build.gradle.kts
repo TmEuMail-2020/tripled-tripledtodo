40255e36467b64d48817ae9e34b979f497104f75
khofmans <kris@rotateit.be>
2021-02-21 09:57:13 +0100

    Introduce validation module to validate user input

---
+++ code/infrastructure/incoming/validation/build.gradle.kts
@@ -0,0 +1,34 @@
+val kotest_version: String by project
+
+plugins {
+    val springVersion = "2.4.1"
+
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot") version springVersion
+    id("io.spring.dependency-management")
+    id("jacoco")
+}
+
+dependencies {
+    implementation(project(":code:vocabulary"))
+    implementation(project(":code:application:api"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+}
+
+tasks {
+    bootJar {
+        enabled = false
+    }
+
+    jar {
+        enabled = true
+    }
+}
